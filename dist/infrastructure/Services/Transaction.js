ef8e4180fda9dc6798d3d8734208aabdab931c04
Tarek Eslam <89143141+tarek1o@users.noreply.github.com>
2023-12-29 20:17:38 +0200

    remove async keyword from transact method in Transaction class

--- dist/infrastructure/Services/Transaction.js
+++ dist/infrastructure/Services/Transaction.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Transaction = void 0;
 const db_1 = __importDefault(require("../../domain/db"));
 class Transaction {
-    static async transact(fn) {
+    static transact(fn) {
         try {
-            return await db_1.default.$transaction(fn);
+            return db_1.default.$transaction(fn);
         }
         catch (error) {
             if (error.code !== 'P2025') {
                 throw error;
             }
-            return await Transaction.transact(fn);
+            return Transaction.transact(fn);
         }
     }
     ;
--- src/infrastructure/Services/Transaction.ts
+++ src/infrastructure/Services/Transaction.ts
@@ -1,14 +1,14 @@
 import prisma from "../../domain/db";
 
 export abstract class Transaction {
-  static async transact<T>(fn: () => Promise<T>): Promise<T>{
+  static transact<T>(fn: () => Promise<T>): Promise<T>{
 		try {
-			return await prisma.$transaction(fn);
+			return prisma.$transaction(fn);
 		}catch(error: any) {
 			if(error.code !== 'P2025') {
 				throw error;
 			}
-			return await Transaction.transact<T>(fn);
+			return Transaction.transact<T>(fn);
 		}
 	};
 }
