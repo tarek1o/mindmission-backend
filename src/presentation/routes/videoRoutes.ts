13f5a1d1d8bdc846c9e91192e123629310e54541
Tarek Eslam <89143141+tarek1o@users.noreply.github.com>
2024-02-11 18:58:42 +0200

    change build command

--- src/presentation/routes/videoRoutes.ts
+++ src/presentation/routes/videoRoutes.ts
@@ -4,10 +4,12 @@ import {idValidation} from "../middlewares/express-validator/idValidation";
 import {Authorization} from '../middlewares/authorization-validator/AuthorizationValidator';
 import {addVideoValidation, updateVideoValidation} from "../middlewares/express-validator/videoValidator"
 import { VideoController } from '../controllers/VideoController';
+import multer from 'multer';
+import { VideoUploader } from '../services/VideoUploader';
 
 const {isAuthenticated, isAuthorized} = container.get<Authorization>('Authorization');
 const {getAllVideos, getVideoById, createVideo, updateVideo, deleteVideo} = container.get<VideoController>('VideoController');
-
+const {upload} = new VideoUploader();
 const videoRouter = express.Router();
 
 videoRouter.route("/get")
@@ -24,5 +26,11 @@ videoRouter.route("/update/:id")
 
 videoRouter.route("/delete/:id")
 	.post(idValidation, isAuthenticated, isAuthorized('Video', 'DELETE'), deleteVideo);
+
+const storage = multer.memoryStorage();
+const multerUpload = multer({ storage });
+
+videoRouter.route('/upload')
+	.post(multerUpload.single('video'), upload)
 
 export default videoRouter;
